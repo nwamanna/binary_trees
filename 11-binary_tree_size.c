cefc5348f07bc4f74744cfdffd71bee1a920e4c4
Nwamanna <nwamannazika@gmail.com>
2023-06-01 21:57:15 +0100

    binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,12 @@
-#include "binary_trees
+#include "binary_trees.h"
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+	if (tree == NULL)
+		return 0;
+   	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	return (left + right + 1);
+}
+
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,12 +1,27 @@
 #include "binary_trees.h"
+/**
+* binary_tree_height - returns height of tree
+* @tree: pointer to tree
+*
+* Return: height of tree
+*/
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height;
-    size_t right_height;
-    if (tree == NULL || (tree->left && tree->right))
-        return 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    return (left_height > right_height ? left_height : right_height) + 1;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	else
+		left_height = 0;
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		right_height = 0;
+	return (left_height > right_height ? left_height : right_height);
 }
